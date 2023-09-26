a39a19ba6aa2f8fc3e358bd5b663b5f8d30ec3b1
SonuChaudhary <siddhantc982@gmail.com>
2023-09-26 18:45:38 +0530

    Task_17 Modified

--- BACKEND_DEV/Express_JS/Task_17/app.js
+++ BACKEND_DEV/Express_JS/Task_17/app.js
@@ -1,21 +1,24 @@
-const path = require('path');
+// const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const sequelize = require('./util/DB_Connection')
 
 const errorController = require('./controllers/error');
+const cors = require('cors')
 
 const app = express();
+app.use(cors())
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+// import routes
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -24,7 +27,6 @@ app.use(errorController.get404);
 
 sequelize.sync()
     .then(result => {
-        // console.log(result)
         app.listen(7048);
     })
     .catch(err => {
