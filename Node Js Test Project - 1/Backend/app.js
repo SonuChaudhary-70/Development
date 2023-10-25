60b7f8ad4c0e0da3b9a0e34256e3c18bd7e97bb2
SonuChaudhary <siddhantc982@gmail.com>
2023-10-25 11:04:01 +0530

    Main server file added (Backend server)

--- Node Js Test Project - 1/Backend/app.js
+++ Node Js Test Project - 1/Backend/app.js
@@ -4,13 +4,30 @@ const sequelize = require('./util/db_connection')
 const bodyParser = require('body-parser')
 const postRoutes = require('./routes/post');
 const commentRoutes = require('./routes/comment');
+const Post = require('./model/post');
+const Comment = require('./model/comment');
 const cors = require('cors')
 
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    Post.findAll()
+    .then(post =>{
+        req.post = post
+        next()
+    })
+    .catch(err =>{
+        console.log('Error while fetching post: ' + err.message);
+    })
+})
+
 app.use('/post', postRoutes)
-app.use('/comment', commentRoutes)
+app.use('/comment', commentRoutes);
+
+// define relationship b/w post and comment
+Post.hasMany(Comment,{ as: 'comments' });
+Comment.belongsTo(Post,{ constraints: true, onDelete: 'CASCADE' });
 
 sequelize
     // .sync({force:true})
