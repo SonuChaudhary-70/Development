6678585718d93f3f089373d4fc24f77b01f155df
SonuChaudhary <siddhantc982@gmail.com>
2023-10-27 12:46:20 +0530

    Backend server file added with project info

---
+++ BACKEND_DEVELOPMENT/Expense Tracker - Node Js Project/BACKEND/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const signUpRoutes = require('./router/user');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const sequelize = require('./util/dbConfig')
+
+// middleware used for all routes
+app.use(cors());
+app.use(bodyParser.json());
+
+// user routes for sign-up and login
+app.use('/user', signUpRoutes)
+
+// server creation and connecting db with server
+sequelize.sync({ force: true })
+    .then(() => {
+        console.log('\nserver is listening on port : 4001');
+        app.listen(4001)
+    })
+    .catch(err => {
+        console.log('Error while creating server', err);
+    })
