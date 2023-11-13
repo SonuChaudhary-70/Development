66447c9c982478aafafc2353a17f137a5e4db2de
SonuChaudhary <siddhantc982@gmail.com>
2023-11-13 15:48:02 +0530

    login backend added

--- BACKEND_DEVELOPMENT/Expense Tracker - Node Js Project/BACKEND/util/dbConfig.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('expense_tracker', 'root', 'SIDc78', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
-
-module.exports = sequelize;
---
+++ BACKEND_DEVELOPMENT/Expense Tracker - Node Js Project/expense_tracker/controller/user.js
@@ -0,0 +1,67 @@
+const User = require('../model/user');
+const bcrypt = require("bcrypt")
+
+function isStringInvalid(string) {
+    return !!(string == undefined || string.length === 0);
+}
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    let user = await User.findOne({ where: { email: email } })
+    try {
+        if (!user) {
+            return res.status(404).json({ success: false, message: 'User not found' })
+        }
+        // user found now validate the user
+        let isValidUser = await bcrypt.compare(password, user.password);
+        if (!isValidUser) {
+            return res.status(401).json({ success: false, message: 'Password not correct' })
+        }
+        // res.redirect('/homePage')
+        return res.status(200).json({ success: true, message: "User logged in successfully", authorized: true })
+    }
+    catch (error) {
+        console.log('Error while login :', error);
+    }
+}
+
+exports.signUp = async (req, res, next) => {
+    const { username, email, password } = req.body;
+    const saltRounds = 10
+    if (isStringInvalid(username) || isStringInvalid(email || isStringInvalid(password))) {
+        return res.status(400).json({ err: "Bad parameters. Something is missing" })
+    }
+    try {
+        // check if user exist with same email id or not
+        let user = await User.findOne({ where: { email: email } });
+        if (!user) {
+            let hashedPassword = await bcrypt.hash(password,saltRounds)
+                User.create({
+                    username: username,
+                    email: email,
+                    password: hashedPassword
+                });
+                if(!hashedPassword) throw new Error('Error while hashing password')
+                return res.status(200).json({ success: true, message: "User created successfully" })
+        } else {
+            return res.status(409).json({ success: false, message: "Email already exists" });
+        }
+    }
+    catch (err) {
+        console.log('Error while creating a user :', err);
+    }
+}
+
+exports.isUserExist = async (req, res) => {
+    let user = await User.findOne({ where: { email: req.params.id } });
+    try {
+        if (user) {
+            return res.status(200).json({ success: true, message: "user found", user: user });
+        } else {
+            return res.status(409).json({ success: false, message: 'user not found ' })
+        }
+    }
+    catch (err) {
+        console.log("Error in checking the user", err);
+    }
+}
