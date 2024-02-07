8b0e7dd3d48dcd69dd48b57693263cbfa814cfe2
SonuChaudhary <siddhantc982@gmail.com>
2024-02-07 23:36:31 +0530

    Typescript Demo

---
+++ BACKEND_DEVELOPMENT/typescript/app.ts
@@ -0,0 +1,14 @@
+const num1ele = document.getElementById('num1') as HTMLInputElement;
+const num2ele = document.getElementById('num2') as HTMLInputElement;
+const buttonElement = document.querySelector('button') ;
+function add(num1: number, num2: number) {
+    return num1 + num2;
+}
+
+buttonElement.addEventListener('click',()=>{
+    const num1 = num1ele.value
+    const num2 = num2ele.value
+    const result = add(+num1, +num2);
+    console.log(result);
+    
+})
